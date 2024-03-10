d97bf6ba43f4e6f56c2f641fe800c010edf10880
majidH <majid.hadadian@gmail.com>
2024-03-10 12:01:09 +0330

    Lifting State Up and Add Results component

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Result from "./components/Result";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
   return (
     <>
       <Header />
-      <UserInput/>
+      <UserInput onChange={handleChange} userInput={userInput}/>
+      <Result input={userInput} />
     </>
   );
 }
